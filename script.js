a5ff82ccc4d83f2ce5bae678e730fba3d2982a13
Jores1-web <joreshouetin@gmail.com>
2025-09-19 18:40:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -30,4 +30,22 @@ backToTop.addEventListener("click", () => {
 window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
-
+// Fermer le menu burger lorsqu'un lien est cliqué
+        document.addEventListener('DOMContentLoaded', function() {
+            var navLinks = document.querySelectorAll('.nav-link');
+            var navbarToggler = document.querySelector('.navbar-toggler');
+            var navbarCollapse = document.querySelector('.navbar-collapse');
+            
+            navLinks.forEach(function(navLink) {
+                navLink.addEventListener('click', function() {
+                    // Vérifier si le menu burger est visible (en mode mobile)
+                    if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                        // Fermer le menu burger
+                        var bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+                            toggle: false
+                        });
+                        bsCollapse.hide();
+                    }
+                });
+            });
+        });
