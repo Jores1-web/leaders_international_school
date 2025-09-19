80c9b69560b7c8e87ba34a47b363427cea83402c
Jores1-web <joreshouetin@gmail.com>
2025-09-19 11:19:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-  const counters = document.querySelectorAll('.counter');
-  counters.forEach(counter => {
-    const updateCount = () => {
-      const target = +counter.getAttribute('data-target');
-      const current = +counter.innerText.replace(/\D/g,'');
-      const increment = target / 100; // vitesse
-      
-      if (current < target) {
-        counter.innerText = Math.ceil(current + increment) + (target === 100 ? "%" : "+");
-        setTimeout(updateCount, 30);
-      } else {
-        counter.innerText = target + (target === 100 ? "%" : "+");
-      }
-    };
-    updateCount();
-  });
+const counters = document.querySelectorAll('.counter');
+counters.forEach(counter => {
+const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const current = +counter.innerText.replace(/\D/g,'');
+    const increment = target / 100; // vitesse
+    
+    if (current < target) {
+    counter.innerText = Math.ceil(current + increment) + (target === 100 ? "%" : "+");
+    setTimeout(updateCount, 30);
+    } else {
+    counter.innerText = target + (target === 100 ? "%" : "+");
+    }
+};
+updateCount();
+});
+
+
+const backToTop = document.getElementById("backToTop");
+
+window.onscroll = function() {
+if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    backToTop.style.display = "block";
+} else {
+    backToTop.style.display = "none";
+}
+};
+
+backToTop.addEventListener("click", () => {
+window.scrollTo({ top: 0, behavior: 'smooth' });
+});
